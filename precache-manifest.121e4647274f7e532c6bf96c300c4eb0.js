0cd4a777cd75c509be523fa1e7d77042703a242e
Minji Kim <wisdomin121@naver.com>
2021-06-07 17:44:15 +0900

    Updates

---
+++ precache-manifest.121e4647274f7e532c6bf96c300c4eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532c69a1874a656ee027b1372a38efe8",
+    "url": "/React_stock/index.html"
+  },
+  {
+    "revision": "56fa1899c25cd83301b1",
+    "url": "/React_stock/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6312b8f12a76e2dc9d83",
+    "url": "/React_stock/static/js/2.344e804e.chunk.js"
+  },
+  {
+    "revision": "e83ce7034a5af4445bbf9607dcb2bb1d",
+    "url": "/React_stock/static/js/2.344e804e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56fa1899c25cd83301b1",
+    "url": "/React_stock/static/js/main.df413f20.chunk.js"
+  },
+  {
+    "revision": "136501a7009d075f1bdd",
+    "url": "/React_stock/static/js/runtime-main.42f06afe.js"
+  }
+]);
